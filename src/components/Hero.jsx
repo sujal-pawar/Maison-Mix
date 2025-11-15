8f0322e814cb54e4dc47746c5761b2a7a6919620
Sujal Pawar <sujalpawar00007@gmail.com>
2025-11-15 19:23:53 +0530

    completed about section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,7 +45,7 @@ const Hero = () => {
             }
         }).to('.right-leaf', {y: 200}, 0).to('.left-leaf', {y: -200}, 0)    
 
-        const startValue = isMobile?'top 50%':'center 60%';
+        const startValue = isMobile?'top 60%':'center 60%';
         const endValue = isMobile?'120% top':'bottom top';
         // Video scroll animation timeline
         const tl = gsap.timeline({
